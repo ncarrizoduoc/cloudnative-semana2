56f7dfa08a05e686603b93a1a9cd32f835177967
ncarrizoduoc <ni.carrizoc@duocuc.cl>
2026-05-29 03:44:43 -0400

    Se elimina import no utilizado

--- src/main/java/com/duoc/inscripciones/controller/InscripcionController.java
+++ src/main/java/com/duoc/inscripciones/controller/InscripcionController.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.SerializationUtils;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
